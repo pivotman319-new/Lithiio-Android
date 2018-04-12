03f68e3107c27eaf88d40b97b937dd4eab67fccf
MilkGames <memegames1337@gmail.com>
2018-04-12 19:52:40 +0200

    update platform www

--- platforms/android/app/src/main/assets/www/index.js
+++ platforms/android/app/src/main/assets/www/index.js
@@ -8,21 +8,29 @@ function onDeviceReady() {
     if (!localStorage.getItem("history")) {
     	localStorage.setItem("history", '[]')
     };
-    if (localStorage.getItem("pendingupload")) {
-    	window.isIntent = true;
-    	uploadf(localStorage.getItem("pendingupload"));
-    }
     var ft;
     window.isIntent = false;
     window.plugins.intent.setNewIntentHandler(function(Intent) {
         if (Intent.clipItems[0].uri) {
             window.isIntent = true;
             uploadf(Intent.clipItems[0].uri);
-            delete Intent;
         }
     });
 };
 
+var uhistory = JSON.parse(localStorage.getItem("history"));
+if (uhistory.length == 0) {
+	document.getElementById("table").insertAdjacentHTML("beforebegin", "<h3>No uploads :(</h3>");
+} else {
+	var imgs = ["png", "jpg", "jpeg", "gif"];
+	for (var i = 0; i < uhistory.length; i++) {
+		if ( imgs.includes( uhistory[i].url.split('.').pop() ) ) {
+			document.getElementById("table").insertAdjacentHTML("afterbegin", "<tr> <td><div class=imageblock><a href=" + uhistory[i].url + "><img class=thumb src=" + uhistory[i].url + "><div class=type>Picture </a><img class=link src=link.png></div><div class=timestamp datetime=" + uhistory[i].time + "></div></div></td></tr>");
+		} else {
+			document.getElementById("table").insertAdjacentHTML("afterbegin", "<tr> <td><div class=imageblock><a href=" + uhistory[i].url + "><img class=thumb src=file.png><div class=type>File (." + uhistory[i].url.split('.').pop() + ")</a> <img class=link src=link.png></div><div class=timestamp datetime=" + uhistory[i].time + "></div></div></td></tr>");
+		}
+}}
+timeago().render(document.querySelectorAll('.timestamp'));
 
 function uploadf(URI) {
     cordova.plugins.firebase.analytics.logEvent("upload", {intent: window.isIntent});
@@ -88,7 +96,9 @@ function openUp() {
 }
 
 function logout() {
-    localStorage.removeItem("apikey");
-    localStorage.removeItem("history");
-    window.location.href = "setup.html";
+	if (confirm("Are you SURE you want to logout? This will clear your upload history!")) {
+		localStorage.removeItem("apikey");
+    	localStorage.removeItem("history");
+    	window.location.href = "setup.html";
+	}
 }
