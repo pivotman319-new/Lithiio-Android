4bd172dfe69a20616405a2c69c194b98d2321ffb
MilkGames <memegames1337@gmail.com>
2018-04-12 19:46:10 +0200

    1.0.7 More progress (proper UI soon)

--- platforms/android/app/src/main/assets/www/index.js
+++ platforms/android/app/src/main/assets/www/index.js
@@ -5,12 +5,20 @@ function onDeviceReady() {
     if (!localStorage.getItem("apikey")) {
         window.location.href = "setup.html";
     };
+    if (!localStorage.getItem("history")) {
+    	localStorage.setItem("history", '[]')
+    };
+    if (localStorage.getItem("pendingupload")) {
+    	window.isIntent = true;
+    	uploadf(localStorage.getItem("pendingupload"));
+    }
     var ft;
     window.isIntent = false;
     window.plugins.intent.setNewIntentHandler(function(Intent) {
         if (Intent.clipItems[0].uri) {
             window.isIntent = true;
             uploadf(Intent.clipItems[0].uri);
+            delete Intent;
         }
     });
 };
@@ -38,18 +46,25 @@ function uploadf(URI) {
                     document.getElementById("progb").value = "0";
                     cordova.plugins.clipboard.copy(JSON.parse(result.response).url);
                     window.isIntent = false;
+                    var obj = JSON.parse(localStorage.getItem("history"));
+					obj.push({url: JSON.parse(result.response).url, time: new Date()});
+					jsonStr = JSON.stringify(obj);
+					localStorage.setItem("history", jsonStr);
+					localStorage.removeItem("pendingupload");
                 } else {
                     document.getElementById("message").removeAttribute("href");
                     document.getElementById("message").innerHTML = JSON.parse(result.response).error;
                     document.getElementById("onlu").style.visibility = 'hidden';
                     document.getElementById("progb").value = "0";
                     window.isIntent = false;
+                    localStorage.removeItem("pendingupload");
                 }
             }, function(error) {
                 document.getElementById("onlu").style.visibility = 'hidden';
                 document.getElementById("progb").value = "0";
                 console.log(JSON.stringify(error));
                 window.isIntent = false;
+                localStorage.removeItem("pendingupload");
             }, options);
             ft.onprogress = function(progressEvent) {
                 if (progressEvent.lengthComputable) {
@@ -74,5 +89,6 @@ function openUp() {
 
 function logout() {
     localStorage.removeItem("apikey");
+    localStorage.removeItem("history");
     window.location.href = "setup.html";
 }
--- www/index.js
+++ www/index.js
@@ -5,6 +5,9 @@ function onDeviceReady() {
     if (!localStorage.getItem("apikey")) {
         window.location.href = "setup.html";
     };
+    if (!localStorage.getItem("history")) {
+    	localStorage.setItem("history", '[]')
+    };
     var ft;
     window.isIntent = false;
     window.plugins.intent.setNewIntentHandler(function(Intent) {
@@ -15,6 +18,19 @@ function onDeviceReady() {
     });
 };
 
+var uhistory = JSON.parse(localStorage.getItem("history"));
+if (uhistory.length == 0) {
+	document.getElementById("table").insertAdjacentHTML("beforebegin", "<h3>No uploads :(</h3>");
+} else {
+	var imgs = ["png", "jpg", "jpeg", "gif"];
+	for (var i = 0; i < uhistory.length; i++) {
+		if ( imgs.includes( uhistory[i].url.split('.').pop() ) ) {
+			document.getElementById("table").insertAdjacentHTML("afterbegin", "<tr> <td><div class=imageblock><a href=" + uhistory[i].url + "><img class=thumb src=" + uhistory[i].url + "><div class=type>Picture </a><img class=link src=link.png></div><div class=timestamp datetime=" + uhistory[i].time + "></div></div></td></tr>");
+		} else {
+			document.getElementById("table").insertAdjacentHTML("afterbegin", "<tr> <td><div class=imageblock><a href=" + uhistory[i].url + "><img class=thumb src=file.png><div class=type>File (." + uhistory[i].url.split('.').pop() + ")</a> <img class=link src=link.png></div><div class=timestamp datetime=" + uhistory[i].time + "></div></div></td></tr>");
+		}
+}}
+timeago().render(document.querySelectorAll('.timestamp'));
 
 function uploadf(URI) {
     cordova.plugins.firebase.analytics.logEvent("upload", {intent: window.isIntent});
@@ -38,18 +54,25 @@ function uploadf(URI) {
                     document.getElementById("progb").value = "0";
                     cordova.plugins.clipboard.copy(JSON.parse(result.response).url);
                     window.isIntent = false;
+                    var obj = JSON.parse(localStorage.getItem("history"));
+					obj.push({url: JSON.parse(result.response).url, time: new Date()});
+					jsonStr = JSON.stringify(obj);
+					localStorage.setItem("history", jsonStr);
+					localStorage.removeItem("pendingupload");
                 } else {
                     document.getElementById("message").removeAttribute("href");
                     document.getElementById("message").innerHTML = JSON.parse(result.response).error;
                     document.getElementById("onlu").style.visibility = 'hidden';
                     document.getElementById("progb").value = "0";
                     window.isIntent = false;
+                    localStorage.removeItem("pendingupload");
                 }
             }, function(error) {
                 document.getElementById("onlu").style.visibility = 'hidden';
                 document.getElementById("progb").value = "0";
                 console.log(JSON.stringify(error));
                 window.isIntent = false;
+                localStorage.removeItem("pendingupload");
             }, options);
             ft.onprogress = function(progressEvent) {
                 if (progressEvent.lengthComputable) {
@@ -73,6 +96,9 @@ function openUp() {
 }
 
 function logout() {
-    localStorage.removeItem("apikey");
-    window.location.href = "setup.html";
+	if (confirm("Are you SURE you want to logout? This will clear your upload history!")) {
+		localStorage.removeItem("apikey");
+    	localStorage.removeItem("history");
+    	window.location.href = "setup.html";
+	}
 }
