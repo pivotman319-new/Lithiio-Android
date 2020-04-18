37508e6f6e5ea3f246611b34cc96f9af878c1bc7
pivotman319 <pivotman2003@gmail.com>
2020-04-18 21:18:02 +0200

    update apikey param to setItem to api_key, upgrade to new v2 fetch api

--- platforms/android/app/src/main/assets/www/setup.js
+++ platforms/android/app/src/main/assets/www/setup.js
@@ -7,7 +7,7 @@ var data = new FormData();
   if (this.readyState === 4) {
     var r = JSON.parse(this.responseText);
 	if (r.apikey) {
-		localStorage.setItem("apikey", r.apikey);
+		localStorage.setItem("api_key", r.apikey);
 		window.location.replace("index.html");
 	} else {
 		document.getElementById("error").innerHTML = r.error;
@@ -20,6 +20,6 @@ function login() {
 	var password = document.getElementById("password").value;
 	data.append("email", email);
 	data.append("password", password);
-	xhr.open("POST", "https://lithi.io/api/v1/fetch-api-key.php");
+	xhr.open("POST", "https://lithi.io/api/v2/fetch-api-key.php");
 	xhr.send(data);
 };
